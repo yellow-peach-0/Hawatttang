b373b1b18a18440508238e93ecdfe02c4d9ffeab
teamSeeSun <94329521+teamSeeSun@users.noreply.github.com>
2021-12-09 22:31:32 +0900

    identity 되돌아오기 기능 추가

--- 황도.js
+++ 황도.js
@@ -1,10 +1,23 @@
 const display = document.querySelector("#slideBox")
 const buttonL = document.querySelector("#L")
 const buttonR = document.querySelector("#R")
+let current = 1;
 
 buttonL.addEventListener("click", () => {
-  display.style.transform = "translate(0)"
+  if (current == 1) {
+    display.style.transform = "translate(-100vw)"
+    current = 2;
+  } else if (current == 2) {
+    display.style.transform = "translate(0)"
+    current = 1;
+  }
 })
 buttonR.addEventListener("click", () => {
-  display.style.transform = "translate(-100vw)"
+  if (current == 1) {
+    display.style.transform = "translate(-100vw)"
+    current = 2;
+  } else if (current == 2) {
+    display.style.transform = "translate(0)"
+    current = 1;
+  }
 })
