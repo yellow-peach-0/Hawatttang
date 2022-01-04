568cc3efe4ac14e503a072b2ef0555db9d7ab995
teamSeeSun <94329521+teamSeeSun@users.noreply.github.com>
2022-01-04 11:47:48 +0900

    미디어쿼리 적용, 경고문 추가

--- 삼실.js
+++ 삼실.js
@@ -2,21 +2,21 @@
 /*아이콘 애니메이션*/
 var index = 0;
 window.onload = function () {
-  slideShow();
+   slideShow();
 }
 
 function slideShow() {
-  var i;
-  var x = document.getElementsByClassName("icon_slide");
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";
-  }
-  index++;
-  if (index > x.length) {
-    index = 1;
-  }
-  x[index - 1].style.display = "block";
-  setTimeout(slideShow, 800);
+   var i;
+   var x = document.getElementsByClassName("icon_slide");
+   for (i = 0; i < x.length; i++) {
+      x[i].style.display = "none";
+   }
+   index++;
+   if (index > x.length) {
+      index = 1;
+   }
+   x[index - 1].style.display = "block";
+   setTimeout(slideShow, 800);
 
 }
 
@@ -30,304 +30,314 @@ const n_circle4 = document.querySelector("#n_circle4")
 const n_circle5 = document.querySelector("#n_circle5")
 const n_circle6 = document.querySelector("#n_circle6")
 const allContainer = document.querySelector('.allContainer');
+const title = document.querySelector("#title").getBoundingClientRect().y
+const info = document.querySelector("#info").getBoundingClientRect().y
+const process = document.querySelector("#process").getBoundingClientRect().y
+const value = document.querySelector("#vision").getBoundingClientRect().y
+const menual = document.querySelector("#ds_concept").getBoundingClientRect().y
+const credit = document.querySelector("#credit").getBoundingClientRect().y
 
 
-
-
-    
-  allContainer.addEventListener('scroll', function(){
-    console.log(allContainer.scrollTop)
-
-    if (allContainer.scrollTop >= 0) {
+function defaultcolor() {
+   if (allContainer.scrollTop = title) {
       n_circle1.style.backgroundColor = '#FEE010'
-      n_circle1.addEventListener('mouseover', function(){
-      n_circle1.style.backgroundColor ='#FEE010'
-   });
-     n_circle1.addEventListener('mouseout', function(){
-      n_circle1.style.backgroundColor = '#FEE010'
-   });
-    }
-
-    if(allContainer.scrollTop <=680){
-    n_circle1.addEventListener('mouseover', function(){
-      n_circle1.style.backgroundColor ='#FEE010'
-   });
-     n_circle1.addEventListener('mouseout', function(){
-      n_circle1.style.backgroundColor = '#FEE010'
-   });
-      n_circle2.addEventListener('mouseover', function(){
-      n_circle2.style.backgroundColor ='#FEE010'
-   });
-         n_circle2.addEventListener('mouseout', function(){
-      n_circle2.style.backgroundColor = 'white'
-   });
-      n_circle2.style.backgroundColor = 'white'
-    }
-
-
-
-    if(allContainer.scrollTop >=680){
-      n_circle1.addEventListener('mouseover', function(){
-      n_circle1.style.backgroundColor ='#FEE010'
-   });
-         n_circle1.addEventListener('mouseout', function(){
+   } else {
       n_circle1.style.backgroundColor = 'white'
-   });
+   }
+}
+
+allContainer.addEventListener('scroll', function () {
+   if (allContainer.scrollTop >= title) {
+      n_circle1.style.backgroundColor = '#FEE010'
+      n_circle1.addEventListener('mouseover', function () {
+         n_circle1.style.backgroundColor = '#FEE010'
+      });
+      n_circle1.addEventListener('mouseout', function () {
+         n_circle1.style.backgroundColor = '#FEE010'
+      });
+   }
+
+   if (allContainer.scrollTop <= 680) {
+      n_circle1.addEventListener('mouseover', function () {
+         n_circle1.style.backgroundColor = '#FEE010'
+      });
+      n_circle1.addEventListener('mouseout', function () {
+         n_circle1.style.backgroundColor = '#FEE010'
+      });
+      n_circle2.addEventListener('mouseover', function () {
+         n_circle2.style.backgroundColor = '#FEE010'
+      });
+      n_circle2.addEventListener('mouseout', function () {
+         n_circle2.style.backgroundColor = 'white'
+      });
+      n_circle2.style.backgroundColor = 'white'
+   }
+
+
+
+   if (allContainer.scrollTop >= 680) {
+      n_circle1.addEventListener('mouseover', function () {
+         n_circle1.style.backgroundColor = '#FEE010'
+      });
+      n_circle1.addEventListener('mouseout', function () {
+         n_circle1.style.backgroundColor = 'white'
+      });
       n_circle1.style.backgroundColor = 'white'
-    }
+   }
 
 
 
-   
-    if (allContainer.scrollTop >=680) {
+
+   if (allContainer.scrollTop >= 680) {
       n_circle2.style.backgroundColor = '#FEE010'
-      n_circle2.addEventListener('mouseover', function(){
-      n_circle2.style.backgroundColor ='#FEE010'
-   });
-     n_circle2.addEventListener('mouseout', function(){
+      n_circle2.addEventListener('mouseover', function () {
+         n_circle2.style.backgroundColor = '#FEE010'
+      });
+      n_circle2.addEventListener('mouseout', function () {
+         n_circle2.style.backgroundColor = '#FEE010'
+      });
       n_circle2.style.backgroundColor = '#FEE010'
-   });
-      n_circle2.style.backgroundColor = '#FEE010'
-    }
+   }
 
-  if(allContainer.scrollTop >=2100){
-                  n_circle2.addEventListener('mouseover', function(){
-      n_circle2.style.backgroundColor ='#FEE010'
-   });
-         n_circle2.addEventListener('mouseout', function(){
+   if (allContainer.scrollTop >= 2100) {
+      n_circle2.addEventListener('mouseover', function () {
+         n_circle2.style.backgroundColor = '#FEE010'
+      });
+      n_circle2.addEventListener('mouseout', function () {
+         n_circle2.style.backgroundColor = 'white'
+      });
       n_circle2.style.backgroundColor = 'white'
-   });
-      n_circle2.style.backgroundColor = 'white'
-    }
+   }
 
 
-    if (allContainer.scrollTop >= 2100) {
-        n_circle3.style.backgroundColor = '#FEE010'
-      n_circle3.addEventListener('mouseover', function(){
-      n_circle3.style.backgroundColor ='#FEE010'
-   });
-     n_circle3.addEventListener('mouseout', function(){
+   if (allContainer.scrollTop >= 2100) {
       n_circle3.style.backgroundColor = '#FEE010'
-   });
+      n_circle3.addEventListener('mouseover', function () {
+         n_circle3.style.backgroundColor = '#FEE010'
+      });
+      n_circle3.addEventListener('mouseout', function () {
+         n_circle3.style.backgroundColor = '#FEE010'
+      });
       n_circle3.style.backgroundColor = '#FEE010'
-    }
+   }
 
-    if(allContainer.scrollTop <= 2100){
-                  n_circle3.addEventListener('mouseover', function(){
-      n_circle3.style.backgroundColor ='#FEE010'
-   });
-         n_circle3.addEventListener('mouseout', function(){
+   if (allContainer.scrollTop <= 2100) {
+      n_circle3.addEventListener('mouseover', function () {
+         n_circle3.style.backgroundColor = '#FEE010'
+      });
+      n_circle3.addEventListener('mouseout', function () {
+         n_circle3.style.backgroundColor = 'white'
+      });
       n_circle3.style.backgroundColor = 'white'
-   });
+   }
+
+   if (allContainer.scrollTop >= 3000) {
+      n_circle3.addEventListener('mouseover', function () {
+         n_circle3.style.backgroundColor = '#FEE010'
+      });
+      n_circle3.addEventListener('mouseout', function () {
+         n_circle3.style.backgroundColor = 'white'
+      });
       n_circle3.style.backgroundColor = 'white'
-    }
-
-    if(allContainer.scrollTop >=3000){
-                      n_circle3.addEventListener('mouseover', function(){
-      n_circle3.style.backgroundColor ='#FEE010'
-   });
-         n_circle3.addEventListener('mouseout', function(){
-      n_circle3.style.backgroundColor = 'white'
-   });
-      n_circle3.style.backgroundColor = 'white'
-    }
+   }
 
 
 
-    if(allContainer.scrollTop >=3000){
-        n_circle4.style.backgroundColor = '#FEE010'
-      n_circle4.addEventListener('mouseover', function(){
-      n_circle4.style.backgroundColor ='#FEE010'
-   });
-     n_circle4.addEventListener('mouseout', function(){
+   if (allContainer.scrollTop >= 3000) {
       n_circle4.style.backgroundColor = '#FEE010'
-   });
+      n_circle4.addEventListener('mouseover', function () {
+         n_circle4.style.backgroundColor = '#FEE010'
+      });
+      n_circle4.addEventListener('mouseout', function () {
+         n_circle4.style.backgroundColor = '#FEE010'
+      });
       n_circle4.style.backgroundColor = '#FEE010'
-    }
-    if(allContainer.scrollTop <=3000){
-         n_circle4.addEventListener('mouseover', function(){
-      n_circle4.style.backgroundColor ='#FEE010'
-   });
-         n_circle4.addEventListener('mouseout', function(){
+   }
+   if (allContainer.scrollTop <= 3000) {
+      n_circle4.addEventListener('mouseover', function () {
+         n_circle4.style.backgroundColor = '#FEE010'
+      });
+      n_circle4.addEventListener('mouseout', function () {
+         n_circle4.style.backgroundColor = 'white'
+      });
       n_circle4.style.backgroundColor = 'white'
-   });
+   }
+
+   if (allContainer.scrollTop >= 5100) {
+      n_circle4.addEventListener('mouseover', function () {
+         n_circle4.style.backgroundColor = '#FEE010'
+      });
+      n_circle4.addEventListener('mouseout', function () {
+         n_circle4.style.backgroundColor = 'white'
+      });
       n_circle4.style.backgroundColor = 'white'
-    }
-
-    if(allContainer.scrollTop >=5100){
-         n_circle4.addEventListener('mouseover', function(){
-      n_circle4.style.backgroundColor ='#FEE010'
-   });
-         n_circle4.addEventListener('mouseout', function(){
-      n_circle4.style.backgroundColor = 'white'
-   });
-      n_circle4.style.backgroundColor = 'white'
-    }
+   }
 
 
-   
 
 
-    if (allContainer.scrollTop >=5100) {
-        n_circle5.style.backgroundColor = '#FEE010'
-      n_circle5.addEventListener('mouseover', function(){
-      n_circle5.style.backgroundColor ='#FEE010'
-   });
-     n_circle5.addEventListener('mouseout', function(){
+
+   if (allContainer.scrollTop >= 5100) {
       n_circle5.style.backgroundColor = '#FEE010'
-   });
+      n_circle5.addEventListener('mouseover', function () {
+         n_circle5.style.backgroundColor = '#FEE010'
+      });
+      n_circle5.addEventListener('mouseout', function () {
+         n_circle5.style.backgroundColor = '#FEE010'
+      });
       n_circle5.style.backgroundColor = '#FEE010'
 
-    }
-    if(allContainer.scrollTop <=5100){
-          n_circle5.addEventListener('mouseover', function(){
-      n_circle5.style.backgroundColor ='#FEE010'
-   });
-         n_circle5.addEventListener('mouseout', function(){
+   }
+   if (allContainer.scrollTop <= 5100) {
+      n_circle5.addEventListener('mouseover', function () {
+         n_circle5.style.backgroundColor = '#FEE010'
+      });
+      n_circle5.addEventListener('mouseout', function () {
+         n_circle5.style.backgroundColor = 'white'
+      });
       n_circle5.style.backgroundColor = 'white'
-   });
+   }
+
+
+
+   if (allContainer.scrollTop >= 10100) {
+      n_circle5.addEventListener('mouseover', function () {
+         n_circle5.style.backgroundColor = '#FEE010'
+      });
+      n_circle5.addEventListener('mouseout', function () {
+         n_circle5.style.backgroundColor = 'white'
+      });
       n_circle5.style.backgroundColor = 'white'
-    }
-   
-
-
-    if(allContainer.scrollTop >=10100){
-               n_circle5.addEventListener('mouseover', function(){
-      n_circle5.style.backgroundColor ='#FEE010'
-   });
-         n_circle5.addEventListener('mouseout', function(){
-      n_circle5.style.backgroundColor = 'white'
-   });
-      n_circle5.style.backgroundColor = 'white'
-    }
+   }
 
 
 
-    if (allContainer.scrollTop >= 10100) { n_circle1.style.backgroundColor = '#FEE010'
-      n_circle6.addEventListener('mouseover', function(){
-      n_circle6.style.backgroundColor ='#FEE010'
-   });
-     n_circle6.addEventListener('mouseout', function(){
+   if (allContainer.scrollTop >= 10100) {
+      n_circle1.style.backgroundColor = '#FEE010'
+      n_circle6.addEventListener('mouseover', function () {
+         n_circle6.style.backgroundColor = '#FEE010'
+      });
+      n_circle6.addEventListener('mouseout', function () {
+         n_circle6.style.backgroundColor = '#FEE010'
+      });
       n_circle6.style.backgroundColor = '#FEE010'
-   });
-      n_circle6.style.backgroundColor = '#FEE010'
 
-    }
-    if(allContainer.scrollTop <=10100){
-         n_circle6.addEventListener('mouseover', function(){
-      n_circle6.style.backgroundColor ='#FEE010'
-   });
-         n_circle6.addEventListener('mouseout', function(){
-      n_circle6.style.backgroundColor = 'white'
-   });
-      n_circle6.style.backgroundColor = 'white'
-    }
-
-       if(allContainer.scrollTop <=6100){
+   }
+   if (allContainer.scrollTop <= 10100) {
+      n_circle6.addEventListener('mouseover', function () {
+         n_circle6.style.backgroundColor = '#FEE010'
+      });
+      n_circle6.addEventListener('mouseout', function () {
+         n_circle6.style.backgroundColor = 'white'
+      });
       n_circle6.style.backgroundColor = 'white'
    }
 
-   if(allContainer.scrollTop >=6100){
+   if (allContainer.scrollTop <= 6100) {
+      n_circle6.style.backgroundColor = 'white'
+   }
+
+   if (allContainer.scrollTop >= 6100) {
       n_circle1.style.backgroundColor = '#13682f'
-         n_circle1.addEventListener('mouseover', function(){
-         n_circle1.style.backgroundColor ='#FEE010'
+      n_circle1.addEventListener('mouseover', function () {
+         n_circle1.style.backgroundColor = '#FEE010'
       });
-         n_circle1.addEventListener('mouseout', function(){
+      n_circle1.addEventListener('mouseout', function () {
          n_circle1.style.backgroundColor = '#13682f'
       });
       n_circle2.style.backgroundColor = '#13682f'
-         n_circle2.addEventListener('mouseover', function(){
-         n_circle2.style.backgroundColor ='#FEE010'
+      n_circle2.addEventListener('mouseover', function () {
+         n_circle2.style.backgroundColor = '#FEE010'
       });
-         n_circle2.addEventListener('mouseout', function(){
+      n_circle2.addEventListener('mouseout', function () {
          n_circle2.style.backgroundColor = '#13682f'
       });
       n_circle3.style.backgroundColor = '#13682f'
-               n_circle3.addEventListener('mouseover', function(){
-         n_circle3.style.backgroundColor ='#FEE010'
+      n_circle3.addEventListener('mouseover', function () {
+         n_circle3.style.backgroundColor = '#FEE010'
       });
-         n_circle3.addEventListener('mouseout', function(){
+      n_circle3.addEventListener('mouseout', function () {
          n_circle3.style.backgroundColor = '#13682f'
       });
       n_circle4.style.backgroundColor = '#13682f'
-               n_circle4.addEventListener('mouseover', function(){
-         n_circle4.style.backgroundColor ='#FEE010'
+      n_circle4.addEventListener('mouseover', function () {
+         n_circle4.style.backgroundColor = '#FEE010'
       });
-         n_circle4.addEventListener('mouseout', function(){
+      n_circle4.addEventListener('mouseout', function () {
          n_circle4.style.backgroundColor = '#13682f'
       });
-  
+
       n_circle6.style.backgroundColor = '#13682f'
-               n_circle6.addEventListener('mouseover', function(){
-         n_circle6.style.backgroundColor ='#FEE010'
+      n_circle6.addEventListener('mouseover', function () {
+         n_circle6.style.backgroundColor = '#FEE010'
       });
-         n_circle6.addEventListener('mouseout', function(){
+      n_circle6.addEventListener('mouseout', function () {
          n_circle6.style.backgroundColor = '#13682f'
       });
    }
 
-   if(allContainer.scrollTop >=6700){
+   if (allContainer.scrollTop >= 6700) {
+      n_circle1.style.backgroundColor = 'white'
+      n_circle1.addEventListener('mouseover', function () {
+         n_circle1.style.backgroundColor = '#FEE010'
+      });
+      n_circle1.addEventListener('mouseout', function () {
          n_circle1.style.backgroundColor = 'white'
-         n_circle1.addEventListener('mouseover', function(){
-         n_circle1.style.backgroundColor ='#FEE010'
-      });
-         n_circle1.addEventListener('mouseout', function(){
-         n_circle1.style.backgroundColor = 'white'
       });
 
-         n_circle2.style.backgroundColor = 'white'
-         n_circle2.addEventListener('mouseover', function(){
-         n_circle2.style.backgroundColor ='#FEE010'
+      n_circle2.style.backgroundColor = 'white'
+      n_circle2.addEventListener('mouseover', function () {
+         n_circle2.style.backgroundColor = '#FEE010'
       });
-         n_circle2.addEventListener('mouseout', function(){
+      n_circle2.addEventListener('mouseout', function () {
          n_circle2.style.backgroundColor = 'white'
       });
 
-         n_circle3.style.backgroundColor = 'white'
-         n_circle3.addEventListener('mouseover', function(){
-         n_circle3.style.backgroundColor ='#FEE010'
+      n_circle3.style.backgroundColor = 'white'
+      n_circle3.addEventListener('mouseover', function () {
+         n_circle3.style.backgroundColor = '#FEE010'
       });
-         n_circle3.addEventListener('mouseout', function(){
+      n_circle3.addEventListener('mouseout', function () {
          n_circle3.style.backgroundColor = 'white'
       });
 
+      n_circle4.style.backgroundColor = 'white'
+      n_circle4.addEventListener('mouseover', function () {
+         n_circle4.style.backgroundColor = '#FEE010'
+      });
+      n_circle4.addEventListener('mouseout', function () {
          n_circle4.style.backgroundColor = 'white'
-         n_circle4.addEventListener('mouseover', function(){
-         n_circle4.style.backgroundColor ='#FEE010'
       });
-         n_circle4.addEventListener('mouseout', function(){
-         n_circle4.style.backgroundColor = 'white'
+      n_circle6.style.backgroundColor = 'white'
+      n_circle6.addEventListener('mouseover', function () {
+         n_circle6.style.backgroundColor = '#FEE010'
       });
-         n_circle6.style.backgroundColor = 'white'
-         n_circle6.addEventListener('mouseover', function(){
-         n_circle6.style.backgroundColor ='#FEE010'
-      });
-         n_circle6.addEventListener('mouseout', function(){
+      n_circle6.addEventListener('mouseout', function () {
          n_circle6.style.backgroundColor = 'white'
       });
    }
 
-     if(allContainer.scrollTop >=10100){
-      n_circle5.addEventListener('mouseover', function(){
-      n_circle5.style.backgroundColor ='#FEE010'
-   });
-         n_circle5.addEventListener('mouseout', function(){
+   if (allContainer.scrollTop >= 10100) {
+      n_circle5.addEventListener('mouseover', function () {
+         n_circle5.style.backgroundColor = '#FEE010'
+      });
+      n_circle5.addEventListener('mouseout', function () {
+         n_circle5.style.backgroundColor = 'white'
+      });
       n_circle5.style.backgroundColor = 'white'
-   });
-      n_circle5.style.backgroundColor = 'white'
 
-      n_circle6.addEventListener('mouseover', function(){
-      n_circle6.style.backgroundColor ='#FEE010'
-   });
-         n_circle6.addEventListener('mouseout', function(){
+      n_circle6.addEventListener('mouseover', function () {
+         n_circle6.style.backgroundColor = '#FEE010'
+      });
+      n_circle6.addEventListener('mouseout', function () {
+         n_circle6.style.backgroundColor = '#FEE010'
+      });
       n_circle6.style.backgroundColor = '#FEE010'
-   });
-      n_circle6.style.backgroundColor = '#FEE010'
-    }
+   }
 
 
-  });
+});
 
-    
- 
+defaultcolor();
+
 
